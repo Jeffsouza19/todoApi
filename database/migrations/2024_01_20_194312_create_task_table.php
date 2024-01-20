4716b8b322471c66dbc88c4f289b96c72d033cd7
Jeferson Souza <jeffsouza.edif@gmail.com>
2024-01-20 17:26:58 -0300

    configurando model task, populacionando o banco com seeders, criando migration para task

---
+++ database/migrations/2024_01_20_194312_create_task_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_task');
+            $table->text('description_task');
+            $table->foreignIdFor(User::class)->references('id')->on('users');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function(){
     return response()->json('pong');
+});
+
+Route::get('/users', function(){
+    $users = User::all();
+    $tasks = Task::all();
+
+    return response()->json([$users, $tasks]);
 });
